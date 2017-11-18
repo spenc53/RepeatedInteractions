4be0a66c8dc7d0b2cf2ebcb3f5e4562b22e07b4a
Anderillo <anderillohale@gmail.com>
2017-11-18 16:08:49 -0700

    Fixed Our Algorithm.

--- src/players/AI/OurAlgorithmPlayer.java
+++ src/players/AI/OurAlgorithmPlayer.java
@@ -12,17 +12,20 @@ public class OurAlgorithmPlayer extends Player {
     public OurAlgorithmPlayer(boolean rowPlayer, Board board) {
         super(rowPlayer, board);
         int max = Integer.MIN_VALUE;
-        this.choice = -1;
+        int location[] = {-1, -1};
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 int square = board.getScore(i, j).getRowScore() - board.getScore(i, j).getColScore();
                 square *= rowPlayer ? 1 : -1;
-                if (square > max) {
+                if (square == max 
+                    && rowPlayer ? board.getScore(i, j).getRowScore() : board.getScore(i, j).getColScore() > rowPlayer ? board.getScore(location[0], location[1]).getRowScore : board.getScore(location[0], location[1]).getColScore()
+                    || square > max) {
                     max = square;
-                    this.choice = i;
+                    location = {i, j};
                 }
             }
         }
+        this.choice = rowPlayer ? location[0] : location[1];
     }
 
     @Override
